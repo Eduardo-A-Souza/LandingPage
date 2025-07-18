00447737165ccdf48e2f77eec6a440f71aac364b
Eduardo <eduardo161205@gmail.com>
2025-07-18 10:27:24 -0300

    finalização da pagina de UnderConstruction e pequenas alterações no componente logo

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -2,11 +2,11 @@
 import React from 'react';
 import './Logo.css';
 
-const Logo = ({ src = "", alt = "Logo_Astem", className = "" }) => {
+const Logo = ({ imgSrc = "", alt = "Logo_Astem", className = "", imgClassName = "" }) => {
     return (
         <div className={`logo-astem img-fluid px-3 ${className}`}>
             <a href="/">
-                <img src={src} alt={alt} />
+                <img src={imgSrc} alt={alt} className={imgClassName} />
             </a>
         </div>
     );
--- src/pages/UnderConstruction/UnderConstruction.js
+++ src/pages/UnderConstruction/UnderConstruction.js
@@ -10,10 +10,11 @@ function UnderConstruction() {
     return (
         <CustomContainer className="vh-100">
             <Logo
-                className="mb-5 h-25 w-50 justify-content-center"
-                src="/images/Logo_astem_main.png"
+                className="mb-5 justify-content-center"
+                imgSrc="/images/Logo_astem_main.png"
+                imgClassName="logo-custom"
             />
-            <MainContainer className="main-custom">
+            <MainContainer className="main-custom w-50">
                 <h1>Página em desenvolvimento</h1>
             </MainContainer>
         </CustomContainer>
